51e586859b63b5378a3bb68cedcd551357369fd9
kameron johnson <kameronjohnson@gmail.com>
2017-12-06 17:01:32 -0800

    add initial components, get youtube api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyC_JEMPx05tHASPi5cCzW1wRRT4nhpWAjs';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div>React Youtube Search</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('App'));
