7cea0c906e2b7f74ebd2ddf1db9f52918f2b9f4a
kameron johnson <kameronjohnson@gmail.com>
2017-12-06 17:44:48 -0800

    add search-bar component functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
@@ -9,8 +9,22 @@ import VideoDetail from './components/video-detail';
 
 const API_KEY = 'AIzaSyC_JEMPx05tHASPi5cCzW1wRRT4nhpWAjs';
 
-const App = () => {
-  return <div>React Youtube Search</div>;
+YTSearch({key: API_KEY, term: 'guitar'}, data => {
+  console.log(data);
+});
+
+class App extends Component {
+  constructor() {
+    super()
+
+  }
+  render() {
+    return (
+      <div>React Youtube Search
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('App'));
